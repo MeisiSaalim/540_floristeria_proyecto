d8b6ded9b2c017294b36b19357940301e89c2317
MeisiSaalim <141336739+MeisiSaalim@users.noreply.github.com>
2023-08-07 13:38:40 +0200

    Update Item.js

--- floristeria-dulces-petalos/src/components/Item.js
+++ floristeria-dulces-petalos/src/components/Item.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Item({ product }) {
   return (
     <div className="item">
-      <img src={product.imgUrl} alt={product.name} />
-      <h3>{product.name}</h3>
-      <p>{product.binomialName}</p>
-      <p>Precio: {product.price} €</p>
+        <Link to={`/product/${product.id}`}>
+            <img src={product.imgUrl} alt={product.name} />
+            <h3>{product.name}</h3>
+            <p>{product.binomialName}</p>
+            <p>Precio: {product.price} €</p>
+        </Link>
     </div>
   );
 }
